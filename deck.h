d1774d7a2b8dab55130862530fd809d97d1f7864
seb <shumatesebastian@gmail.com>
2025-09-30 10:14:48 -0400

    Add Card class

--- deck.h
+++ deck.h
@@ -1,6 +1,16 @@
 #ifndef DECK_H
 #define DECK_H
 
-
+class Card {
+    public:
+        enum Suit { Hearts, Diamonds, Clubs, Spades };
+        enum Rank { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+        Card(Suit suit, Rank rank) : suit(suit), rank(rank) {}
+        Suit getSuit() const { return suit; }
+        Rank getRank() const { return rank; } 
+    private: 
+        Suit suit;
+        Rank rank;
+};
 
 #endif
