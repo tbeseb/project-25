912c363b11a99cffc8b12c3b9cc9d2208ffb35e8
seb <shumatesebastian@gmail.com>
2025-10-01 12:35:20 -0400

    Created the game and added functionality

--- game.cpp
+++ game.cpp
@@ -1,1 +1,85 @@
+#include <iosteam>
+#include "game.h"
+#include "deck.h"
+#include "player.h"
 
+using namespace std;
+
+Game:Game() {
+    deck.shuffle();
+}
+
+void Game::play() {
+    bool keepPlaying = true;
+
+    while (keepPlaying) {
+        newRound();
+        playerTurn();
+        dealerTurn();
+        determineWinner();
+        keepPlaying = ui.askPlayAgain();
+    }
+}
+
+void Game::newRound() {
+    player.clearHand();
+    dealer.clearHand();
+    if (deck.isEmpty()) {
+        deck = deck();
+        deck.shuffle();
+    }
+
+    player.addCard(deck.dealCard());
+    dealer.addCard(deck.dealCard());
+    player.addCard(deck.dealCard());
+    dealer.addCard(deck.dealCard());
+}
+
+void Game::playerTurn() {
+    bool playerBusted = false;
+    while (true) {
+        ui.displayHands(player, dealer, false);
+        if (player.getHandValue() == 21) {
+            cout << "Blackjack! You have 21." << endl;
+            break;
+        }
+        if (player.getHandValue() > 21) {
+            cout << "Bust! You exceeded 21." << endl;
+            playerBusted = true;
+            break;
+        }
+        if (ui.askHitOrStand()) {
+            player.addCard(deck.dealCard());
+        } else {
+            break;
+        }
+    }
+}
+
+void Game::dealerTurn() {
+   ui.showDealerHand(dealer);
+
+   while (dealer.getHandValue() < 17) {
+       dealer.addCard(deck.dealCard());
+       ui.showDealerHand(dealer);
+   }
+}
+
+void Game::determineWinner() {
+    int playerValue = player.getHandValue();
+    int dealerValue = dealer.getHandValue();
+
+    ui.displayHands(player, dealer, true);
+
+    if (playerValue > 21) {
+        cout << "You busted! Dealer wins." << endl;
+    } else if (dealerValue > 21) {
+        cout << "Dealer busted! You win!" << endl;
+    } else if (playerValue > dealerValue) {
+        cout << "You win!" << endl;
+    } else if (playerValue < dealerValue) {
+        cout << "Dealer wins." << endl;
+    } else {
+        cout << "It's a tie!" << endl;
+    }
+}
--- game.h
+++ game.h
@@ -1,4 +1,23 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "deck.h"
+#include "player.h"
+#include "dealer.h"
+#include "UI.h"
+
+class game{
+public:
+    game();
+    void start();
+    void playerTurn();
+    void dealerTurn();
+    void determineWinner();
+    void newRound();
+private:
+    Deck deck;
+    Player player;
+    Dealer dealer;
+    UI ui;
+}
 #endif
